39cec720f3047e698643ab1821e1b59870a43d66
unknown <dridiala88@gmail.com>
2024-04-18 11:35:32 +0100

    init project

--- src/main/java/com/pfe/authsecurity/SafeCareApplication.java
+++ src/main/java/com/pfe/authsecurity/SafeCareApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthsecurityApplication {
+public class SafeCareApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthsecurityApplication.class, args);
+		SpringApplication.run(SafeCareApplication.class, args);
 	}
 
 }
